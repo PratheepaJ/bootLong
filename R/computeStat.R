a02776d546861a3650fdf031dcd8084026fc0f69
Pratheepa Jeganathan <pjeganat@stanford.edu>
2018-04-23 13:22:16 -0700

    computeStat computes betas

--- R/computeStat.R
+++ R/computeStat.R
@@ -8,17 +8,96 @@
 #'
 #' @param ps Observed \code{phyloseq} class object.
 #' @param factors vector of factor variable(s) in the sample data of ps.
+#' @param time Time variable at repeated observations.
+#' @param b numeric, optimal block size to account for dependence within-subject.
 #'
 #' @return dataframe with the last column corresponds to taxa names
-#' and other columns are for moderated coefficients for each factors, including Intercept.
+#' and other columns are for estimated coefficients for each factors using GEE, including Intercept.
 #'
 #' @import "edgeR"
 #' @import "phyloseq"
 #' @import "DESeq2"
 #' @import "limma"
 #' @import "ashr"
-#' @import "EBSeq"
 #' @export
+computeStat <- function(ps,factors,time,b){
+    ot <- as.matrix(otu_table(ps))
+    samdf <- data.frame(sample_data(ps))
+    anno <- data.frame(tax_table(ps))
+    dgeList <- edgeR::DGEList(counts=ot, genes=anno, samples = samdf)
+
+    #   setting up the model
+    des <- as.formula(paste("~", paste(factors, collapse="+")))
+    mm <- model.matrix(des,data=samdf)
+
+    #   to estimate the size factors
+    # #   median-of-ratios
+    #sizefa <- EBSeq::MedianNorm(ot)
+    geo.mean.raw <- apply((ot+1),1,function(x){exp(sum(log(x))/length(x))})
+    sj.com <- 0
+    for(i in 1:dim(ot)[2]){
+        sj.com[i] <- median(ot[,i]/geo.mean.raw)
+    }
+
+    pse <- ps
+    otu_table(pse) <- otu_table(ps)+1
+    pDE <- suppressMessages(phyloseq_to_deseq2(pse,design=des))
+    #rm(pse)
+    pDE <- DESeq2::estimateSizeFactors(pDE)
+    sj <- DESeq2::sizeFactors(pDE)
+
+    # View(data.frame(sj.com,sj))
+
+    v <- arcsinhTransform(counts=dgeList, design=mm, lib.size=sj,plot = F)
+
+    df.beta.hat <- list()
+    for(i in 1:ntaxa(ps)){
+        # df.per.taxa <- data.frame(samdf,otu=as.numeric(t(ot[1,])),sj=sj,weights=v$weights[1,])
+        df.per.taxa <- data.frame(samdf,otu=as.numeric(t(v$E[i,])),sj=sj,weights=v$weights[i,])
+
+        #   need numeric values for Subject ID
+
+        df.per.taxa$idvar <- as.numeric(as.factor(df.per.taxa$SubjectID))
+        df.per.taxa <- arrange(df.per.taxa,SubjectID)
+
+        #   compute residuals
+        # gee1 <- geeglm(otu~Preterm, data=df.per.taxa,id=idvar,corstr = "independence",weights = df.per.taxa$weights,waves = df.per.taxa$Time,offset = df.per.taxa$sj,family = poisson(link = log))
+        gee1 <- geeglm(otu~Preterm, data=df.per.taxa,id=idvar,corstr = "independence",weights = df.per.taxa$weights,waves = df.per.taxa$Time,offset = df.per.taxa$sj,family = gaussian)
+        res <- df.per.taxa$otu-as.vector(residuals(gee1))
+
+        #   compute sample correlation
+        df.per.taxa$res <- res
+        df.per.taxa$Time <- as.factor(df.per.taxa$Time)
+        df.per.s <- df.per.taxa %>% group_by(Time) %>% summarise(meanr=mean(res))
+        acf.res <- as.numeric(acf(df.per.s$meanr,plot = F)$acf)
+        acf.res[(b+1):length(acf.res)] <- 0
+
+        workCorr <- matrix(nrow=length(acf.res),ncol = length(acf.res))
+        for(rw in 1:length(acf.res)){
+            for(nc in 1:length(acf.res)){
+                workCorr[rw,nc] <- acf.res[(abs(rw-nc)+1)]
+            }
+        }
+
+        zcor <- fixed2Zcor(workCorr,id=df.per.taxa$idvar,waves = df.per.taxa$Time)
+        #   gee with the working correlation
+        # gee2 <- geeglm(otu~Preterm, data=df.per.taxa,id=idvar,corstr = "fixed",weights = v$weights[1,],waves = df.per.taxa$Time,offset = sj,family = poisson(link = log),zcor=zcor)
+        gee2 <- geeglm(otu~Preterm, data=df.per.taxa,id=idvar,corstr = "fixed",weights = df.per.taxa$weights,waves = df.per.taxa$Time,offset = df.per.taxa$sj,family = gaussian,zcor=zcor)
+
+        df.beta.hat[[i]] <- t(gee2$coefficients)
+    }
+
+
+    df.beta.hat <- data.frame(do.call("rbind",df.beta.hat))
+
+    ASV <- taxa_names(ps)
+    res <- bind_cols(df.beta.hat,ASV=ASV)
+    # res <- res[,c(2,3)]
+    rm(ps)
+    return(res)
+}
+
+### 1st attempt
 # computeStat <- function(ps,factors){
 #     ot <- as.matrix(otu_table(ps))
 #     anno <- data.frame(tax_table(ps))
@@ -159,91 +238,4 @@
 #     rm(ps)
 #     return(res)
 # }
-
-###### 3rd attempt
-computeStat <- function(ps,factors,time,b){
-    ot <- as.matrix(otu_table(ps))
-    samdf <- data.frame(sample_data(ps))
-    anno <- data.frame(tax_table(ps))
-    dgeList <- edgeR::DGEList(counts=ot, genes=anno, samples = samdf)
-
-    #   setting up the model
-    des <- as.formula(paste("~", paste(factors, collapse="+")))
-    mm <- model.matrix(des,data=samdf)
-
-    #   to estimate the size factors
-    # #   median-of-ratios
-    #sizefa <- EBSeq::MedianNorm(ot)
-    pse <- ps
-    otu_table(pse) <- otu_table(ps)+1
-    pDE <- suppressMessages(phyloseq_to_deseq2(pse,design=des))
-    #rm(pse)
-    pDE <- DESeq2::estimateSizeFactors(pDE)
-    sj <- DESeq2::sizeFactors(pDE)
-
-    v <- arcsinhTransform(counts=dgeList, design=mm, lib.size=sj,plot = F)
-
-    df.beta.hat <- list()
-    for(i in 1:ntaxa(ps)){
-        # df.per.taxa <- data.frame(samdf,otu=as.numeric(t(ot[1,])),sj=sj,weights=v$weights[1,])
-        df.per.taxa <- data.frame(samdf,otu=as.numeric(t(v$E[i,])),sj=sj,weights=v$weights[i,])
-
-        #   need numeric values for Subject ID
-
-        df.per.taxa$idvar <- as.numeric(as.factor(df.per.taxa$SubjectID))
-        df.per.taxa <- arrange(df.per.taxa,SubjectID)
-
-        #   compute residuals
-        # gee1 <- geeglm(otu~Preterm, data=df.per.taxa,id=idvar,corstr = "independence",weights = df.per.taxa$weights,waves = df.per.taxa$Time,offset = df.per.taxa$sj,family = poisson(link = log))
-        gee1 <- geeglm(otu~Preterm, data=df.per.taxa,id=idvar,corstr = "independence",weights = df.per.taxa$weights,waves = df.per.taxa$Time,offset = df.per.taxa$sj,family = gaussian)
-        res <- df.per.taxa$otu-as.vector(residuals(gee1))
-
-        #   compute sample correlation
-        df.per.taxa$res <- res
-        df.per.taxa$Time <- as.factor(df.per.taxa$Time)
-        df.per.s <- df.per.taxa %>% group_by(Time) %>% summarise(meanr=mean(res))
-        acf.res <- as.numeric(acf(df.per.s$meanr,plot = F)$acf)
-        acf.res[(b+1):length(acf.res)] <- 0
-
-        workCorr <- matrix(nrow=length(acf.res),ncol = length(acf.res))
-        for(rw in 1:length(acf.res)){
-            for(nc in 1:length(acf.res)){
-                workCorr[rw,nc] <- acf.res[(abs(rw-nc)+1)]
-            }
-        }
-
-        zcor <- fixed2Zcor(workCorr,id=df.per.taxa$idvar,waves = df.per.taxa$Time)
-        #   gee with the working correlation
-        # gee2 <- geeglm(otu~Preterm, data=df.per.taxa,id=idvar,corstr = "fixed",weights = v$weights[1,],waves = df.per.taxa$Time,offset = sj,family = poisson(link = log),zcor=zcor)
-        gee2 <- geeglm(otu~Preterm, data=df.per.taxa,id=idvar,corstr = "fixed",weights = df.per.taxa$weights,waves = df.per.taxa$Time,offset = df.per.taxa$sj,family = gaussian,zcor=zcor)
-
-        df.beta.hat[[i]] <- t(gee2$coefficients)
-    }
-
-
-    df.beta.hat <- data.frame(do.call("rbind",df.beta.hat))
-
-
-
-
-    # #   estimated coefficients
-    # df.beta.hat <- data.frame(t(gee2$coefficients))
-    #
-    # df.sd.beta <- data.frame(fit$stdev.unscaled)
-    # dfreedom <- fit$df.residual[1]
-    #
-    # #   hist(data.frame(fit$coefficients)$PretermTRUE)
-    # #   empirical Bayes shrinkage of the estimators: ashr::ash()
-    # res <- df.beta.hat
-    # for(i in 1:dim(df.beta.hat)[2]){
-    #     shrinkage_fit <- suppressMessages(ash(df.beta.hat[,i],sebetahat = df.sd.beta[,i],mixcompdist = "normal"))
-    #     res[,i] <- shrinkage_fit$result$PosteriorMean
-    # }
-
-    ASV <- taxa_names(ps)
-    res <- bind_cols(df.beta.hat,ASV=ASV)
-    # res <- res[,c(2,3)]
-    rm(ps)
-    return(res)
-}
 
