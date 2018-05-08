a0064419d8b0d43887f1d4211ed114c16112c84e
Pratheepa Jeganathan <pjeganat@stanford.edu>
2018-05-08 10:34:08 -0700

    computStat works

--- R/computeStat.R
+++ R/computeStat.R
@@ -44,13 +44,13 @@ computeStat <- function(ps,factors,time,b){
                 sj <- as.numeric(sj)
                 we.ind <- as.numeric(we[ind,])
                 dff <- samd
-                dff <- cbind(samd,otu=otu,sj=sj,we=we.ind)
+                dff <- cbind(samd,otu=otu,sj=sj,weig=we.ind)
                 #   need numeric values for Subject ID
                 dff$idvar <- as.numeric(as.factor(dff$SubjectID))
                 dff <- arrange(dff,SubjectID)
 
                 #       negative binomial family with arcsinh link
-                glmft.tx <- glm.nb(des2,data = dff,weights = we,method = "glm.fit",link = arcsinhLink())
+                glmft.tx <- glm.nb(des2,data = dff,weights = weig,method = "glm.fit",link = arcsinhLink())
 
                 #   residuals
                 rese <- as.vector(residuals(glmft.tx))
@@ -60,28 +60,35 @@ computeStat <- function(ps,factors,time,b){
                 dfsub <- dff
                 if(!is.factor(dfsub$Time)){dfsub$Time <- as.factor(dfsub$Time)}
                 dfsub <- dfsub %>% group_by(Time) %>% summarise(meanr=mean(res))
-                acf.res <- as.numeric(acf(dfsub$meanr,plot = F,lag.max = dim(dfsub)[1])$acf)
+                dfsub$Time <- as.numeric(as.character(dfsub$Time))
+                dfsub <- arrange(dfsub,Time)
+                
+                meanr <- ts(dfsub$meanr,start = min(dfsub$Time),end = max(dfsub$Time),frequency = 1)
+                
+                acf.res <- as.numeric(acf(meanr,plot = F,lag.max = length(meanr))$acf)
                 acf.res[(b+1):length(acf.res)] <- 0
-
+                
+                
                 workCorr <- matrix(nrow=length(acf.res),ncol = length(acf.res))
                 for(rw in 1:length(acf.res)){
                         for(nc in 1:length(acf.res)){
                                 workCorr[rw,nc] <- acf.res[(abs(rw-nc)+1)]
                         }
                 }
+                
 
                 if(!is.numeric(dff$Time)){dff$Time <- as.numeric(as.character(dff$Time))}
                 wa <- dff$Time
                 theta <- glmft.tx$theta
                 
-                LinkFun <- function(y) log(y + sqrt(y^2 + 1))
-                VarFun <- function(y){y+(y^2*theta)}
-                InvLink <- function(eta)  0.5*exp(-eta)*(exp(2*eta)-1)
-                InvLinkDeriv <- function(eta) {.5*(exp(eta)+exp(-eta))}
-                FunList <- list(LinkFun,VarFun,InvLink,InvLinkDeriv)
+                # LinkFun <- function(y) log(y + sqrt(y^2 + 1))
+                # VarFun <- function(y){y+(y^2*theta)}
+                # InvLink <- function(eta)  0.5*exp(-eta)*(exp(2*eta)-1)
+                # InvLinkDeriv <- function(eta) {.5*(exp(eta)+exp(-eta))}
+                # FunList <- list(LinkFun,VarFun,InvLink,InvLinkDeriv)
                 
                 init.beta <- as.numeric(glmft.tx$coefficients)
-                fit <- geeM::geem(des2,id=idvar,waves = Time,data = dff,family=FunList,weights = we,corstr = "fixed",corr.mat = workCorr,nodummy=TRUE,init.beta = init.beta,scale.fix = TRUE)
+                fit <- geeM::geem(des2,id=idvar,waves = wa,data = dff,family=arcsinhlstLink(),weights = weig,corstr = "fixed",corr.mat = workCorr,nodummy=TRUE,init.beta = init.beta,scale.fix = TRUE)
 
                 return(fit$beta)
 
