0e8cf6a4ffcccb97934bf342e8020b50e9cc93bd
Pratheepa Jeganathan <pjeganat@stanford.edu>
2018-09-11 17:07:30 -0700

    computStat fitting weights

---
+++ R/asinh_voom.R
@@ -0,0 +1,96 @@
+asinh_voom = function (counts,
+                       design = NULL,
+                       lib.size = NULL,
+                       normalize.method = "none",
+                       span = 0.5,
+                       plot = FALSE,
+                       save.plot = FALSE, ...){
+
+        inv_asinh <- function(x) {
+            y <- 0.5*exp(-x)*(exp(2*x)-1)
+            return(y)
+        }
+
+        out <- list()
+
+        if (is(counts, "DGEList")) {
+            out$genes <- counts$genes
+            out$targets <- counts$samples
+            if (is.null(design) && diff(range(as.numeric(counts$sample$group))) >
+                0)
+                design <- model.matrix(~group, data = counts$samples)
+            if (is.null(lib.size))
+                lib.size <- counts$samples$lib.size * counts$samples$norm.factors
+            counts <- counts$counts
+        }
+        else {
+            isExpressionSet <- suppressPackageStartupMessages(is(counts,
+                                                                 "ExpressionSet"))
+            if (isExpressionSet) {
+                if (length(Biobase::fData(counts)))
+                    out$genes <- Biobase::fData(counts)
+                if (length(Biobase::pData(counts)))
+                    out$targets <- Biobase::pData(counts)
+                counts <- Biobase::exprs(counts)
+            }
+            else {
+                counts <- as.matrix(counts)
+            }
+        }
+        n <- nrow(counts)
+        if (n < 2L)
+            stop("Need at least two genes to fit a mean-variance trend")
+        if (is.null(design)) {
+            design <- matrix(1, ncol(counts), 1)
+            rownames(design) <- colnames(counts)
+            colnames(design) <- "GrandMean"
+        }
+        if (is.null(lib.size))
+            lib.size <- colSums(counts)
+        y <- t(asinh(t(counts)*lib.size))
+        y <- normalizeBetweenArrays(y, method = normalize.method)
+        fit <- lmFit(y, design, ...)
+        if (is.null(fit$Amean))
+            fit$Amean <- rowMeans(y, na.rm = TRUE)
+        sx <- fit$Amean - mean(asinh(lib.size))
+        sy <- sqrt(fit$sigma)
+        allzero <- rowSums(counts) == 0
+        if (any(allzero)) {
+            sx <- sx[!allzero]
+            sy <- sy[!allzero]
+        }
+        l <- lowess(sx, sy, f = span)
+        if (plot) {
+            plot(sx, sy, xlab = "log2( count size + 0.5 )", ylab = "Sqrt( standard deviation )",
+                 pch = 16, cex = 0.25)
+            title("voom: Mean-variance trend")
+            lines(l, col = "red")
+        }
+        f <- approxfun(l, rule = 2)
+        if (fit$rank < ncol(design)) {
+            j <- fit$pivot[1:fit$rank]
+            fitted.values <- fit$coef[, j, drop = FALSE] %*% t(fit$design[,
+                                                                          j, drop = FALSE])
+        }
+        else {
+            fitted.values <- fit$coef %*% t(fit$design)
+        }
+        fitted.cpm <- inv_asinh(fitted.values)
+        fitted.count <- t(t(fitted.cpm) / (lib.size))
+        fitted.logcount <- asinh(fitted.count)
+        w <- 1/f(fitted.logcount)^4
+        dim(w) <- dim(fitted.logcount)
+        out$E <- y
+        out$weights <- w
+        out$design <- design
+        if (is.null(out$targets))
+            out$targets <- data.frame(lib.size = lib.size)
+        else out$targets$lib.size <- lib.size
+        if (save.plot) {
+            out$voom.xy <- list(x = sx, y = sy, xlab = "log2( count size + 0.5 )",
+                                ylab = "Sqrt( standard deviation )")
+            out$voom.line <- l
+        }
+
+    new("EList", out)
+}
--- R/computeStat.R
+++ R/computeStat.R
@@ -7,8 +7,8 @@
 #' for each taxa. The empirical Bayes is applied to compute the shirnkage estimates for each factor.
 #'
 #' @param ps Observed \code{phyloseq} class object.
-#' @param factors vector of factor variable(s) in the sample data of ps.
-#' @param time numeric, time variable at repeated observations.
+#' @param main_factor vector of factor variable(s) in the sample data of ps.
+#' @param time_var numeric, time_var variable at repeated observations.
 #' @param b numeric, optimal block size to account for dependence within-subject.
 #'
 #' @return dataframe with the last column corresponds to taxa names
@@ -17,88 +17,268 @@
 #' @import "edgeR"
 #' @import "phyloseq"
 #' @export
-computeStat <- function(ps,factors,time,b,SubjectID_n="SubjectID"){
+computeStat <- function(ps,
+                        main_factor,
+                        time_var,
+                        subjectID_var,
+                        b){
 
-        otu_full <- as.matrix(round(otu_table(ps),digits = 0))+1
-        samdf <- data.frame(sample_data(ps))
-        if(!is.numeric(samdf[,time])){samdf[,time] <- as.numeric(samdf[,time])}
-        if(!is.factor(samdf[,SubjectID_n])){samdf[,SubjectID_n] <- as.factor(samdf[,SubjectID_n])}
+    if(dim(otu_table(ps))[2] != nsamples(ps)){
+        otu_table(ps) = t(otu_table(ps))
+    }
 
-        anno <- data.frame(tax_table(ps))
+    ot = otu_table(ps) %>% data.frame %>% as.matrix
 
-        dgeList <- edgeR::DGEList(counts=otu_full, genes=anno, samples = samdf)
+    if(!isTRUE(all(ot == floor(ot)))){
+        stop("otu_table entries must be integer")
+    }
 
-        des <- as.formula(paste("~", paste(factors, collapse="+")))
-        mm <- model.matrix(des,data=samdf)
+    #   variance stabilization for negative binomial distribution accounting for library sizes.
+    geo_mean = function(x){
+        val = exp(mean(log(x[x>0])))
+    }
 
-        des2 <- as.formula(paste("otuT","~", paste(factors, collapse="+"),"+","offset(arcsinhLink()$linkfun(allSj))"))
+    geom_mean_row = apply(ot, 1, FUN = geo_mean)
 
-        geo.mean.row <- apply((otu_full+1),1,function(x){exp(sum(log(x))/length(x))})
-        sj <- apply((otu_full+1),2,function(x){median(x/geo.mean.row)})
+    median_ratios = function(x, geom_mean_row){
+        rat = x/geom_mean_row
+        median_rat <- median(rat)
+        return(median_rat)
+    }
 
-        v <- arcsinhTransform(counts=dgeList, design=mm, lib.size=sj)
-        weights.cal <- v$weights
+    # library size normalization factor
+    sj = apply(ot, 2, FUN = median_ratios, geom_mean_row = geom_mean_row)
 
-        taxaLst <- as.list(seq(1,ntaxa(ps)))
+    # ot_trans = t(asinh(t(ot)*sj))
 
-        com_beta <- function(taxIndex,sampleDf,otuDf,allSj,weightDf,desingGEE,b,SubjectID_n,time){
-                otuT <- as.numeric(otuDf[taxIndex,])
-                allSj <- as.numeric(allSj)
-                weightT <- as.numeric(weightDf[taxIndex,])
-                dffT <- cbind(sampleDf,otuT =otuT,allSj=allSj,weightT=weightT)
+    ##   compute weights
 
-                dffT$idvar <- as.numeric(as.factor(dffT[,SubjectID_n]))
-                idvar <- "idvar"
-                dffT <- arrange_(dffT,idvar,time)
+    # samdf = sample_data(ps) %>% data.frame
+    des = as.formula(paste("otuT","~", paste(main_factor, collapse="+"),"+","offset(arcsinhLink()$linkfun(sj))"))
+    #
+    # response_fitted <- function(ind, samdf, ot, sj, des){
+    #     otu = as.numeric(ot[ind,])
+    #     sj = as.numeric(sj)
+    #     dff = mutate(samdf, otu=otu, sj=sj)
+    #     glmft = MASS::glm.nb(des, data = dff, method = "glm.fit", link = arcsinhLink())
+    #     res_fitted = fitted(glmft, "response")
+    #     return(res_fitted)
+    # }
+    #
+    # fited = lapply(seq_len(ntaxa(ps)),function(x){
+    #     response_fitted(x, samdf = samdf, ot = (ot+1), sj = sj, des = des)
+    # })
+    #
+    # fited = data.frame(do.call("rbind",fited))
+    # colnames(fited) = sample_names(ps)
+    # rownames(fited) = taxa_names(ps)
+    #
+    # #   compute weight for each observation using the mean-variance relationship of asinh transformed observations accounting for library size
+    # mean_asv = apply(ot_trans, 1, FUN = mean)
+    # sd_asv = sqrt(apply(ot_trans, 1, FUN = sd))
+    #
+    # allzero = (rowSums(ot) == 0)
+    #
+    # if (any(allzero)) {
+    #     mean_asv = mean_asv[!allzero]
+    #     sd_asv = sd_asv[!allzero]
+    # }
+    #
+    # l = lowess(mean_asv, sd_asv, f = span)
+    #
+    # f = approxfun(l, rule = 2)
+    #
+    # fited_trans = t(asinh(t(fited)/sj))
+    #
+    # w = 1/f(fited_trans)^4
+    # dim(w) = dim(fited_trans)
+    # weights.cal = w
 
-                glmft.tx <- glm.nb(formula=desingGEE,data = dffT,weights = weightT,method = "glm.fit",link = arcsinhLink())
+    samdf = sample_data(ps) %>% data.frame
+    des_v <- as.formula(paste("~", paste(main_factor, collapse="+")))
+    mm <- model.matrix(des_v,data=samdf)
+    v <- asinh_voom(counts=ot, design=mm, lib.size=sj)
+    weights.cal = v$weights
+    #   Estimate regression coefficients
+    com_beta <- function(taxIndex,
+                         sampleDf,
+                         otuDf,
+                         allSj,
+                         weightDf,
+                         desingGEE,
+                         b,
+                         subjectID_var,
+                         time_var){
 
-                rese <- as.vector(residuals(glmft.tx))
+        otuT = as.numeric(otuDf[taxIndex,])
+        allSj = as.numeric(allSj)
+        weightT = as.numeric(weightDf[taxIndex,])
+        dffT = cbind(sampleDf, otuT =otuT, allSj=allSj, weightT=weightT)
 
-                dffT$res <- rese
-                dfsub <- dffT
-                if(!is.factor(dfsub[,time])){dfsub[,time] <- as.factor(dfsub[,time])}
-                meanT <- data.frame(dfsub %>% group_by_(time) %>% summarise(meanr=mean(res)))
+        dffT$idvar = as.numeric(as.factor(dffT[,subjectID_var]))
+        idvar = "idvar"
+        dffT = arrange_(dffT, idvar, time_var)
 
-                if(!is.numeric(meanT[,time])){meanT[,time] <- as.numeric(as.character(meanT[,time]))}
-                meanT <- arrange_(meanT,time)
+        glmft.tx = glm.nb(formula = desingGEE,
+                          data = dffT,
+                          weights = weightT,
+                          method = "glm.fit",
+                          link = arcsinhLink())
 
-                meanr <- ts(meanT$meanr,start = min(meanT[,time]),end = max(meanT[,time]),frequency = 1)
+        #rese = as.vector(residuals(glmft.tx))
+        rese = resid(glmft.tx,"response")
+        rese = t(asinh(t(rese)*sj))
 
-                acf.res <- as.numeric(acf(meanr,plot = F,lag.max = length(meanr))$acf)
-                acf.res[(b+1):length(acf.res)] <- 0
-
-
-                workCorr <- matrix(nrow=length(acf.res),ncol = length(acf.res))
-                for(rw in 1:length(acf.res)){
-                        for(nc in 1:length(acf.res)){
-                                workCorr[rw,nc] <- acf.res[(abs(rw-nc)+1)]
-                        }
-                }
-
-
-                if(!is.numeric(dffT[,time])){dffT[,time] <- as.numeric(as.character(dffT[,time]))}
-                wavesTime <- dffT[,time]
-                idvarV <- dffT[,"idvar"]
-                theta <- glmft.tx$theta
-
-                init.beta <- as.numeric(glmft.tx$coefficients)
-
-                fit <-  tryCatch(geeM::geem(formula=desingGEE,id=idvarV,waves = wavesTime,data = dffT,family=arcsinhlstLink(),corstr = "fixed",weights = weightT,corr.mat = workCorr,init.beta = init.beta,nodummy=TRUE)$beta,error=function(e){t(glmft.tx$coefficients)})
-
-                return(fit)
-
+        dffT$res = rese
+        dfsub = dffT
+        if(!is.factor(dfsub[,time_var])){
+            dfsub[,time_var] = as.factor(dfsub[,time_var])
         }
 
-        df.beta.hat <- lapply(taxaLst,function(x){com_beta(x,sampleDf=samdf,otuDf=otu_full,allSj=sj,weightDf=weights.cal,desingGEE=des2,b=b,SubjectID_n=SubjectID_n,time=time)})
+        meanT = data.frame(dfsub %>% group_by_(time_var) %>% summarise(meanr=mean(res)))
 
-        df.beta.hat <- data.frame(do.call("rbind",df.beta.hat))
+        if(!is.numeric(meanT[,time_var])){
+            meanT[,time_var] <- as.numeric(as.character(meanT[,time_var]))
+        }
 
-        ASV <- taxa_names(ps)
-        res <- bind_cols(df.beta.hat,ASV=ASV)
-        rm(ps)
-        return(res)
+        meanT = arrange_(meanT, time_var)
+
+        meanr = ts(meanT$meanr, start = min(meanT[,time_var]), end = max(meanT[,time_var]), frequency = 1)
+
+        acf.res = as.numeric(acf(meanr, plot = F, lag.max = length(meanr))$acf)
+        acf.res[(b+1):length(acf.res)] = 0
+
+
+        workCorr = matrix(nrow=length(acf.res), ncol = length(acf.res))
+        for(rw in 1:length(acf.res)){
+            for(nc in 1:length(acf.res)){
+                workCorr[rw,nc] = acf.res[(abs(rw-nc)+1)]
+            }
+        }
+
+
+        if(!is.numeric(dffT[,time_var])){
+            dffT[,time_var] = as.numeric(as.character(dffT[,time_var]))
+            }
+        wavesTime = dffT[,time_var]
+        idvarV = dffT[,"idvar"]
+        theta = glmft.tx$theta
+
+        init.beta = as.numeric(glmft.tx$coefficients)
+
+        fit <-  tryCatch(geeM::geem(formula = desingGEE,
+                                    id = idvarV,
+                                    waves = wavesTime,
+                                    data = dffT,
+                                    family=arcsinhlstLink(),
+                                    corstr = "fixed",
+                                    weights = weightT,
+                                    corr.mat = workCorr,
+                                    init.beta = init.beta,
+                                    nodummy=TRUE)$beta,
+                         error = function(e){
+                             t(glmft.tx$coefficients)
+                             })
+
+        return(fit)
+
+    }
+
+    df.beta.hat <- lapply(seq_len(ntaxa(ps)), function(x){
+        com_beta(x,
+                 sampleDf = samdf,
+                 otuDf = (ot+1),
+                 allSj = sj,
+                 weightDf = weights.cal,
+                 desingGEE = des,
+                 b=b,
+                 subjectID_var=subjectID_var,
+                 time_var=time_var)
+        })
+
+    df.beta.hat = data.frame(do.call("rbind", df.beta.hat))
+
+
+
 }
 
 
-
+# otu_full <- as.matrix(round(otu_table(ps),digits = 0))+1
+# samdf <- data.frame(sample_data(ps))
+# if(!is.numeric(samdf[,time_var])){samdf[,time_var] <- as.numeric(samdf[,time_var])}
+# if(!is.factor(samdf[,subjectID_var])){samdf[,subjectID_var] <- as.factor(samdf[,subjectID_var])}
+#
+# anno <- data.frame(tax_table(ps))
+#
+# dgeList <- edgeR::DGEList(counts=otu_full, genes=anno, samples = samdf)
+#
+# des <- as.formula(paste("~", paste(main_factor, collapse="+")))
+# mm <- model.matrix(des,data=samdf)
+#
+# des2 <- as.formula(paste("otuT","~", paste(main_factor, collapse="+"),"+","offset(arcsinhLink()$linkfun(allSj))"))
+#
+# geo.mean.row <- apply((otu_full+1),1,function(x){exp(sum(log(x))/length(x))})
+# sj <- apply((otu_full+1),2,function(x){median(x/geo.mean.row)})
+#
+# v <- arcsinhTransform(counts=dgeList, design=mm, lib.size=sj)
+# weights.cal <- v$weights
+#
+# taxaLst <- as.list(seq(1,ntaxa(ps)))
+#
+# com_beta <- function(taxIndex, sampleDf, otuDf, allSj, weightDf, desingGEE, b, subjectID_var, time_var){
+#     otuT <- as.numeric(otuDf[taxIndex,])
+#     allSj <- as.numeric(allSj)
+#     weightT <- as.numeric(weightDf[taxIndex,])
+#     dffT <- cbind(sampleDf,otuT =otuT,allSj=allSj,weightT=weightT)
+#
+#     dffT$idvar <- as.numeric(as.factor(dffT[,subjectID_var]))
+#     idvar <- "idvar"
+#     dffT <- arrange_(dffT,idvar,time_var)
+#
+#     glmft.tx <- glm.nb(formula=desingGEE,data = dffT,weights = weightT,method = "glm.fit",link = arcsinhLink())
+#
+#     rese <- as.vector(residuals(glmft.tx))
+#
+#     dffT$res <- rese
+#     dfsub <- dffT
+#     if(!is.factor(dfsub[,time_var])){dfsub[,time_var] <- as.factor(dfsub[,time_var])}
+#     meanT <- data.frame(dfsub %>% group_by_(time_var) %>% summarise(meanr=mean(res)))
+#
+#     if(!is.numeric(meanT[,time_var])){meanT[,time_var] <- as.numeric(as.character(meanT[,time_var]))}
+#     meanT <- arrange_(meanT,time_var)
+#
+#     meanr <- ts(meanT$meanr,start = min(meanT[,time_var]),end = max(meanT[,time_var]),frequency = 1)
+#
+#     acf.res <- as.numeric(acf(meanr,plot = F,lag.max = length(meanr))$acf)
+#     acf.res[(b+1):length(acf.res)] <- 0
+#
+#
+#     workCorr <- matrix(nrow=length(acf.res),ncol = length(acf.res))
+#     for(rw in 1:length(acf.res)){
+#         for(nc in 1:length(acf.res)){
+#             workCorr[rw,nc] <- acf.res[(abs(rw-nc)+1)]
+#         }
+#     }
+#
+#
+#     if(!is.numeric(dffT[,time_var])){dffT[,time_var] <- as.numeric(as.character(dffT[,time_var]))}
+#     wavesTime <- dffT[,time_var]
+#     idvarV <- dffT[,"idvar"]
+#     theta <- glmft.tx$theta
+#
+#     init.beta <- as.numeric(glmft.tx$coefficients)
+#
+#     fit <-  tryCatch(geeM::geem(formula=desingGEE,id=idvarV,waves = wavesTime,data = dffT,family=arcsinhlstLink(),corstr = "fixed",weights = weightT,corr.mat = workCorr,init.beta = init.beta,nodummy=TRUE)$beta,error=function(e){t(glmft.tx$coefficients)})
+#
+#     return(fit)
+#
+# }
+#
+# df.beta.hat <- lapply(taxaLst,function(x){com_beta(x, sampleDf=samdf, otuDf=otu_full, allSj=sj, weightDf=weights.cal, desingGEE=des2, b=b, subjectID_var=subjectID_var, time_var=time_var)})
+#
+# df.beta.hat <- data.frame(do.call("rbind",df.beta.hat))
+#
+# ASV <- taxa_names(ps)
+# res <- bind_cols(df.beta.hat,ASV=ASV)
+# rm(ps)
+# return(res)
