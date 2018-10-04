fa02bc68e292c0565f2235f054f25d019de4f7af
Pratheepa Jeganathan <pjeganat@stanford.edu>
2018-10-04 11:45:10 -0700

    update computeStat

--- R/psTransform.R
+++ R/psTransform.R
@@ -55,7 +55,7 @@ psTransform <- function(ps, main_factor) {
         sj <- as.numeric(sj)
         weightT <- as.numeric(weights.cal[ind, ])
         dff <- mutate(samdf, otu = otu, sj = sj, weightT = weightT)
-        dff <- mutate(dff, weightT = ifelse(otu == 0, 0, weightT))
+        #dff <- mutate(dff, weightT = ifelse(otu == 0, 0, weightT))
         glmft <- MASS::glm.nb(des, data = dff, weights = weightT, method = "glm.fit", link = arcsinhLink())
         res_residuals <- resid(glmft)
         rt <- list(res_residuals)
