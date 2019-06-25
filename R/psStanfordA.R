313ef3c8723a31ff1c38152af663d8806e8b078d
Pratheepa Jeganathan <pjeganat@stanford.edu>
2019-06-25 10:29:14 -0700

    making public bootLong

--- R/psStanfordA.R
+++ R/psStanfordA.R
@@ -1,12 +1,12 @@
-#' The Stanford pregnancy data \code{'phyloseq'} object.
+#' The Stanford pregnancy data as \code{\link{phyloseq-class}}.
 #'
-#' A \code{'phyloseq'} class object with 109 taxa from 678 biological samples.
+#' A \code{\link{phyloseq-class}} with 109 taxa from 678 biological samples.
 #'
-#' @format An object of class \code{'phyloseq'}.
+#' @format An object of \code{\link{phyloseq-class}}.
 #' \describe{
-#'   \item{SubjectID}{IDs of individuals}
+#'   \item{SubjectID}{IDs of pregnant women}
 #'   \item{Preterm}{A factor variable with two levels TRUE or FALSE}
-#'   \item{SampleID}{Sample ID for each repeated biological sample}
+#'   \item{SampleID}{Sample ID for repeated biological sample}
 #'   \item{GWColl}{Gestational week at sampling}
 #' }
 #' @source The data use in the paper: \href{https://www.ncbi.nlm.nih.gov/pubmed/26283357}{where}
--- R/pssim.R
+++ R/pssim.R
@@ -1,15 +1,15 @@
-#' Simulated \code{'phyloseq'} object.
+#' Simulated as \code{\link{phyloseq-class}}.
 #'
-#' A \code{'phyloseq'} class object with 50 taxa from 200 biological samples.
+#' A \code{\link{phyloseq-class}} with 50 taxa from 200 biological samples.
 #'
-#' @format An object of class \code{'phyloseq'}.
+#' @format An object of class \code{\link{phyloseq-class}}.
 #' \describe{
 #'   \item{SubjectID}{IDs of individuals}
 #'   \item{Group}{A factor variable with two levels Case or Control}
 #'   \item{SampleID}{Sample ID for each repeated biological sample}
 #'   \item{Time}{Time at repeated biological sample}
 #' }
-#' @source Simulated based on the pregnancy data. Paper on \href{http://www.pnas.org/content/112/35/11060.short}{where}
+#' @source Simulated based on the StanfordA pregnancy data. Paper on \href{http://www.pnas.org/content/112/35/11060.short}{where}
 #' @references Pratheepa Jeganathan (Simulated dataset)
 #' @examples
 #' \dontrun{sample_data(psSim)
